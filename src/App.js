cd2cdb77acee80c3f550af9448a062f69e7a8f16
Pedro Mirassol <114862129+Deathzu7@users.noreply.github.com>
2025-01-21 01:47:33 +0000

    Add structure for cards and item sections

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Navbar from "../src/components/Navbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" exact />
+          <Route path="/" exact element={<Home />}></Route>
         </Routes>
       </Router>
     </>
